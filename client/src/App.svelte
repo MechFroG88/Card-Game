5b15d02f0995494482801abbff4d8501da018dfd
DavidTan0527 <david_weiucheng@hotmail.com>
2021-05-02 14:30:53 +0800

    Update client for websocket

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,33 +1,62 @@
 <script lang="ts">
-  import Card from '@/components/Card.svelte';
+  import { io } from 'socket.io-client';
 
-  export let name:string = "Test";
+  const socket = io("localhost:3000");
+  let msgs:Array<string> = [];
+  let message:string = "";
+
+  socket.on("connect", () => {
+    console.log(`Connected with id: ${socket.id}`);
+  });
+
+  socket.on("disconnect", () => { });
+
+  socket.on("chat message", (messages:Array<string>) => {
+    msgs = messages;
+  });
+
+  function sendMessage() {
+    socket.emit("chat message", message);
+    message = "";
+  }
+
 </script>
 
-<main>
-  <Card cardTitle={name}></Card>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<div class="body">
+  <ul>
+    {#each msgs as msg}
+      <li>{msg}</li>
+    {:else}
+      <h2>Start the chat by sending some messages!</h2>
+    {/each}
+  </ul>
+  <div class="input-area">
+    <input type="text" bind:value={message}>
+    <input type="button" value="Send" on:click={sendMessage}>
+  </div>
+</div>
 
 <style lang="scss">
-  $red: red;
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-    h1 {
-      color: $red;
-      text-transform: uppercase;
-      font-size: 4em;
-      font-weight: 100;
+.body {
+  ul {
+    padding: 2rem;
+    li {
+      list-style: none;
+      margin: .2rem 0;
+      padding: .5rem;
+      border: solid 1px rgba(0,0,0, .2);
+      border-radius: .3rem;
     }
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+    h2 {
+      text-align: center;
+    }
+  }
+  .input-area {
+    display: flex;
+    justify-content: center;
+    input {
+      margin: 0 .5rem;
+    }
+  }
+}
 </style>
